492f8f6d377a24dc66761fe90b288ae1cb2f2e79
Colin Klein <colintklein@gmail.com>
2015-10-26 20:07:25 -0400

    finished the app; not yet tested

--- app/src/main/java/com/colin/simpleweather/WeatherActivity.java
+++ app/src/main/java/com/colin/simpleweather/WeatherActivity.java
@@ -1,11 +1,16 @@
 package com.colin.simpleweather;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -13,17 +18,60 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Use WeatherActivityFragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new WeatherActivityFragment())
+                    .commit();
+        }
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//        });
     }
 
+    // Recognize our only menu option, to change city
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.change_city) {
+            showInputDialog();
+        }
+        return false;
+    }
+
+    // Shows input dialog for city input
+    // Uses AlertDialog.Builder to create a Dialog object
+    // that prompts user to enter name of city.
+    // Input is passed on to changeCity method, which stores
+    // it using CityPreference class and calls the Fragment's
+    // changeCity method
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Change city");
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton("Go", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                changeCity(input.getText().toString());
+            }
+        });
+        builder.show();
+    }
+
+    public void changeCity(String city) {
+        WeatherActivityFragment wf = (WeatherActivityFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.container);
+        wf.changeCity(city);
+        new CityPreference(this).setCity(city);
+    }
 }
--- app/src/main/java/com/colin/simpleweather/WeatherActivityFragment.java
+++ app/src/main/java/com/colin/simpleweather/WeatherActivityFragment.java
@@ -1,22 +1,167 @@
 package com.colin.simpleweather;
 
+import android.graphics.Typeface;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
- * A placeholder fragment containing a simple view.
+ * Fragment containing the main Weather activity.
  */
 public class WeatherActivityFragment extends Fragment {
 
+    // Icon typeface to be used
+    Typeface weatherFont;
+
+    // TextViews from XML layout
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+
+    // Use a Handler object to update user inferface
+    // (cannot update UI from background thread)
+    Handler handler;
+
+    // Initialize Handler object in constructor
     public WeatherActivityFragment() {
+        handler = new Handler();
     }
 
+    // Create and return the View
+    // Initialize the TextViews from XML layout
+    // Set icon to use weatherFont typeface
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weather, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_weather, container, false);
+        cityField = (TextView) rootView.findViewById(R.id.city_field);
+        updatedField = (TextView) rootView.findViewById(R.id.updated_field);
+        detailsField = (TextView) rootView.findViewById(R.id.details_field);
+        currentTemperatureField = (TextView) rootView.findViewById(R.id.current_temperature_field);
+        weatherIcon = (TextView) rootView.findViewById(R.id.weather_icon);
+
+        weatherIcon.setTypeface(weatherFont);
+        return rootView;
+    }
+
+    // Initialize weatherFont object to use Typeface
+    // Invoke updateWeatherData method
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
+        updateWeatherData(new CityPreference(getActivity()).getCity());
+    }
+
+    /**
+     * Since only the main Thread is allowed to update the UI
+     * of an Android app, calling Toast or renderWeather directly
+     * from the background thread would lead to a runtime error.
+     * That is why we call these methods using the Handler's
+     * post method.
+     */
+    // Updates weather data for given city
+    // Displays a Toast error if city not found
+    private void updateWeatherData(final String city) {
+        new Thread() {
+            public void run() {
+                final JSONObject json = RemoteFetch.getJSON(getActivity(), city);
+                if (json == null) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Toast.makeText(getActivity(), getActivity().getString(R.string.place_not_found),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    // Updates the TextView objects
+    private void renderWeather(JSONObject json) {
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US)
+                    + ", " +
+                    json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) +
+                    "\n" + "Humidity: " + main.getString("humidity") + "%"
+                    + "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + " ℉");
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+        } catch (Exception e) {
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
+
+    // Method to map a weather id to an icon
+    // Weather codes in 200 range related to T-storms
+    // Weather codes in 300 range related to drizzles
+    // Weather codes in 500 range signify rain
+    // etc...
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+        int id = actualId/100;
+        String icon = "";
+        if (actualId == 800) {
+            long currentTime = new Date().getTime();
+            if (currentTime >= sunrise && currentTime < sunset) {
+                // is daytime
+                icon = getActivity().getString(R.string.weather_sunny);
+            } else {
+                // is nighttime
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        } else {
+            switch(id) {
+                case 2 : icon = getActivity().getString(R.string.weather_thunder);
+                case 3 : icon = getActivity().getString(R.string.weather_drizzle);
+                case 7 : icon = getActivity().getString(R.string.weather_foggy);
+                case 8 : icon = getActivity().getString(R.string.weather_cloudy);
+                case 6 : icon = getActivity().getString(R.string.weather_snowy);
+                case 5: icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+
+            }
+        }
+        weatherIcon.setText(icon);
+    }
+
+    // Method to let user update their current city
+    public void changeCity(String city) {
+        updateWeatherData(city);
     }
 }
